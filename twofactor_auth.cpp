aac898ecdd05c3cf8cd7a26a8da60ce75db4057f
Ioann Volkov <volkov.ioann@gmail.com>
2014-04-25 15:03:28 +0400

    Some part of algo structure

--- twofactor_auth.cpp
+++ twofactor_auth.cpp
@@ -3,6 +3,30 @@
 #include <syslog.h>
 #include <security/pam_ext.h>
 
+#include <fstream>
+#include <boost/unordered_map.hpp>
+
+typedef boost::unordered_map<std::string, std::string> U2SN_MAP_TYPE;
+
+bool isThereDeviceWithSerial(std::string const& sn, std::string& dev) {
+    // TODO: implement this function
+
+    return false;
+}
+
+bool loadUserToSNMap(U2SN_MAP_TYPE& u2sn) {
+    std::ifstream f("/etc/u2sn");
+    if (!f.good())
+        return false;
+    std::string u, sn;
+    while (f.good()) {
+        f >> u >> sn;
+        u2sn.insert(std::make_pair(u, sn));
+    }
+    f.close();
+    return true;
+}
+
 int pam_sm_authenticate(pam_handle_t* pamh, int flags, int argc, const char** argv) {
     const char* user;
     int res = pam_get_user(pamh, &user, 0);
@@ -11,7 +35,28 @@ int pam_sm_authenticate(pam_handle_t* pamh, int flags, int argc, const char** ar
         return PAM_USER_UNKNOWN;
     }
 
-    // TODO: implement this function
+    U2SN_MAP_TYPE u2sn;
+    if (!loadUserToSNMap(u2sn)) {
+        pam_syslog(pamh, LOG_ERR, "load user-to-serial-number map failed");
+        return PAM_AUTHINFO_UNAVAIL;
+    }
+
+    U2SN_MAP_TYPE::const_iterator it = u2sn.find(user);
+    if (it == u2sn.end()) {
+        pam_syslog(pamh, LOG_ERR, "no user '%s' in user-to-serial-number map", user);
+        return PAM_USER_UNKNOWN;
+    }
+
+    std::string sn = it->second;
+    std::string dev;
+    if (!isThereDeviceWithSerial(sn, dev)) {
+        pam_syslog(pamh, LOG_ERR, "no device with serial number '%s' is connected", sn.c_str());
+        return PAM_CRED_INSUFFICIENT;
+    }
+
+    // TODO: everything else
+
+    //pam_get_authtok()
 
     return PAM_SUCCESS;
 }
