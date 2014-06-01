1aa1e7e69ceeb765ce8d8040816c3c06ebacfa71
Ioann Volkov <volkov.ioann@gmail.com>
2014-06-01 13:48:21 +0400

    Chapter 9

--- report/build.sh
+++ report/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-./clean.sh
-rm *.pdf
+rm *.aux *.log *.toc *.ps *.dvi *.bbl *.blg *.pdf
 echo R | latex report.tex
 bibtex8 -B -c cp1251.csf report # Not: "bibtex8 -B -c sty/cp1251.csf report", cause my cp1251.csf is bad
 echo R | latex report.tex
 echo R | latex report.tex
 dvips -t portrait report.dvi
 ps2pdf report.ps report.pdf
+rm *.aux *.log *.toc *.ps *.dvi *.bbl *.blg
--- report/clean.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm *.aux *.log *.toc *.ps *.dvi *.bbl *.blg
